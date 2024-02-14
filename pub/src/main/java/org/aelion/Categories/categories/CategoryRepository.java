5f06eedc3cb24b1f15c145f6dad13e42241924be
Regitraz <regitrazz@gmail.com>
2024-02-14 15:31:50 +0100

    fix(pub): folder deleted

--- pub/src/main/java/org/aelion/Categories/categories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.aelion.Categories.categories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, String> {
-
-}
