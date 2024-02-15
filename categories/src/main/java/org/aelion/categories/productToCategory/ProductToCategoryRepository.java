ae1538870278b506cc21e95e3ffbfc8834a550ec
Vincent Muffato <vincent.muffato@gmail.com>
2024-02-15 14:15:13 +0100

    Optimized get CategoriesId list by product ean code

--- categories/src/main/java/org/aelion/categories/productToCategory/ProductToCategoryRepository.java
+++ categories/src/main/java/org/aelion/categories/productToCategory/ProductToCategoryRepository.java
@@ -3,7 +3,9 @@ package org.aelion.categories.productToCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductToCategoryRepository extends JpaRepository<ProductToCategory, String> {
-
+    List<ProductToCategory> findByProductId(String productId);
 }
