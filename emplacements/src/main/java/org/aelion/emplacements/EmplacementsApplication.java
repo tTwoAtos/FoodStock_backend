62b5738f851175aefcdb8c822e9cd8fd07e9347c
Regitraz <regitrazz@gmail.com>
2024-02-29 15:15:00 +0100

    feat(emplacement service): api doc added

--- emplacements/src/main/java/org/aelion/emplacements/EmplacementsApplication.java
+++ emplacements/src/main/java/org/aelion/emplacements/EmplacementsApplication.java
@@ -1,5 +1,7 @@
 package org.aelion.emplacements;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -7,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Emplacement API", version = "${springdoc.version}", description = "Documentation Emplacement API v1.0")
+)
 public class EmplacementsApplication {
 
 	public static void main(String[] args) {
