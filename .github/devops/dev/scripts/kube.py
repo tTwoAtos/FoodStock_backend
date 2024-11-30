f6787c5560126313a816279baef0801cef7ccf56
Hamza REBBANI <hamza.rebbani@atos.net>
2024-11-30 14:20:54 +0100

    ci/devops: delete obsolete files

--- .github/devops/dev/scripts/kube.py
+++
@@ -1,9 +0,0 @@
-import kubernetes
-import subprocess
-from dataclasses import dataclass as dtc
-
-class SwitchMinikube():
-    def __init__(self) -> None:
-        pass
-
-    kubernetes.__version__
--- .github/devops/dev/scripts/local-workflow.py
+++
@@ -1,7 +0,0 @@
-"""
-Script objective: 
-to be capable of running locally CI pipeline with Github Actions 
-"""
-
-import subprocess
-from dataclasses import dataclass as dtc
