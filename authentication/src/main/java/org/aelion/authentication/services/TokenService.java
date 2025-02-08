1659a9ed2f038030a9ff4eb8397fee2cead67fba
Regitraz <48715468+regitraz2@users.noreply.github.com>
2025-02-08 17:31:31 +0100

    fix(authentication): added community to token

--- authentication/src/main/java/org/aelion/authentication/services/TokenService.java
+++ authentication/src/main/java/org/aelion/authentication/services/TokenService.java
@@ -1,8 +1,5 @@
 package org.aelion.authentication.services;
 
-import com.nimbusds.jwt.JWTClaimsSet;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import org.aelion.authentication.entity.AuthUserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -13,8 +10,6 @@ import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
 
 @Service
 public class TokenService {
@@ -23,10 +18,11 @@ public class TokenService {
 
     @Autowired
     PasswordEncoder passwordEncoder;
+
     public String generate(AuthUserEntity user, Authentication auth, long duration) {
         Instant now = Instant.now();
 
-        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+        JwtClaimsSet.Builder claimsBuilder = JwtClaimsSet.builder()
                 .issuer("Ornate")
                 .issuedAt(now)
                 .expiresAt(now.plusSeconds(duration))
@@ -34,9 +30,16 @@ public class TokenService {
                 .claim("role", auth.getAuthorities().toString())
                 .claim("firstname", user.getFirstname())
                 .claim("lastname", user.getLastname())
-                .claim("user_id", user.getId())
-                .build();
+                .claim("user_id", user.getId());
 
-        return jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+        if (user.getLoggedInCommunityId() != null) {
+            claimsBuilder.claim("logged_in_community_id", user.getLoggedInCommunityId());
+        }
+
+        return jwtEncoder.encode(
+                JwtEncoderParameters.from(
+                        claimsBuilder.build()
+                )
+        ).getTokenValue();
     }
 }
